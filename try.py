44e87bd100842e8ee982d34c73cba757d7cf20b0
Deependra12 <dg02432018@student.ku.edu.np>
2020-12-02 14:34:51 +0545

    final

--- try.py
+++ try.py
@@ -37,8 +37,6 @@ while(flag ==0):
             print(index)
             flag = 1
                 
-        
-        
     if(flag ==0):
         print("Invalid Calculation point. This point should be nearer to the x_points and can differ only by 0.0001")
 
@@ -49,23 +47,36 @@ print('\nBACKWARD DIFFERENCE TABLE\n')
 for i in range(0,n):
     print('%0.2f' %(x[i]), end='')
     for j in range(0, i+1):
-        print('\t%0.2f' %(y[i][j]), end='')
+        print('\t%0.4f' %(y[i][j]), end='')
     print()
-h = x[1] - x[0]
-print(h)
-    
 
-print(index)
-x=1
+h = x[1] - x[0]
+print("height difference:", h)
+    
 for i in range(1,index+1):
-    print(y[index] [i])
-    term =(y[index][i])/x
-    x=x+1
+    term =(y[index][i])/i
     sum = sum + term
          
 
 first_derivative = round((sum/h),3)
 
 print("Reqd value is",first_derivative)
+
+
+
+sum = 0
+term=0
+pattern = [1,1, 11 / 12, 5 / 6,0.761,0.7,0.648,0.603]
+for i in range(1, index):
+    term= (y[index][i+1] * pattern[i - 1])
+    sum=sum+term
+second_derivative = round((sum / (h * h)), 3)
+print(f'Reqd value of second derivative at point {num} is {second_derivative}')
+
+
+
+
+
+
 
         
